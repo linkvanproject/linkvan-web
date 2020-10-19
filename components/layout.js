6ed75ef2e63435cb614c5b30daa125ebd2d6195a
Andre Souza <andresouza4@gmail.com>
2020-10-18 18:50:20 -0700

    Add Font Roboto & Style Header

--- components/layout.js
+++ components/layout.js
@@ -2,10 +2,16 @@ import React from 'react'
 import Head from 'next/head'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Container from '@material-ui/core/Container'
 import Button from '@material-ui/core/Button'
 import Alert from '@material-ui/lab/Alert'
+import Link from '@material-ui/core/Link'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import theme from '../theme'
 
 const TopAlert = styled(Alert)`
   justify-content: center;
@@ -24,9 +30,44 @@ const Nav = styled.nav`
   padding: 10px;
 `
 
+const LogoBox = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+
+  @media (min-width: 960px) {
+    justify-content: flex-start;
+  }
+`
+
+const Logo = styled.img`
+  max-height: 50px;
+`
+
+const Home = styled(Typography)`
+  padding-left: 10px;
+`
+
+const SearchBox = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin: 8px 0 4px;
+`
+
+const SearchButton = styled(Button)`
+  padding-top: 8px;
+  padding-bottom: 8px;
+  margin-left: 4px;
+`
+
+const SpaceXS = styled.div`
+  padding: 6px 0;
+`
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Head>
         <meta
@@ -42,16 +83,37 @@ const Layout = ({ children }) => {
           </TopAlert>
           <Container>
             <Nav>
-              <div>
-                <span>Logo here</span>
-                <a href="/">Home</a>
-              </div>
-              <div>
-                <input type="text" placeholder="asdasd" />
-                <Button variant="contained" color="primary" disableElevation>
-                  Search
-                </Button>
-              </div>
+              <Grid container>
+                <Grid item xs={12} md={6}>
+                  <LogoBox href="/">
+                    <Logo src="/logo-header.png" alt="Linkvan logo" />
+                    <Home variant="h6">| Home</Home>
+                  </LogoBox>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <SpaceXS />
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <form noValidate autoComplete="off">
+                    <SearchBox>
+                      <TextField
+                        id="search-facilities"
+                        label="Search Facilities"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                      <SearchButton
+                        variant="contained"
+                        color="primary"
+                        disableElevation
+                      >
+                        Search
+                      </SearchButton>
+                    </SearchBox>
+                  </form>
+                </Grid>
+              </Grid>
             </Nav>
           </Container>
         </Header>
@@ -61,7 +123,7 @@ const Layout = ({ children }) => {
           <a href="/">About Us</a> |<a href="/">Sign up</a>
         </footer>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import 'fontsource-roboto'
+
+// eslint-disable-next-line react/prop-types
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
