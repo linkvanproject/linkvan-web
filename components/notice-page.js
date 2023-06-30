3755c130b1c0b441bc20a9263fd7257f892a4b22
Andre Souza <andresouza4@gmail.com>
2023-06-30 10:23:56 -0700

    feat(notice): display covid19 notice

---
+++ components/notice-page.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import Head from 'next/head'
+import formatDate from 'utils/format-date'
+import styled from 'styled-components'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import Paper from '@material-ui/core/Paper'
+import Layout from 'components/layout'
+import NavBar from 'components/nav-bar'
+import PageTitle from 'components/page-title'
+import { Loading } from 'components/icons'
+
+const StyledPaper = styled(Paper)`
+  padding: 12px;
+  margin-bottom: 12px;
+`
+
+const NoticePage = ({ data, error }) => {
+  const SectionTitle = (props) => (
+    <Box fontSize="h6.fontSize" color="grey.700" mb={1} {...props} />
+  )
+
+  const getContent = () => {
+    if (error)
+      return (
+        <Box textAlign="center" flexGrow={1}>
+          failed to load
+        </Box>
+      )
+
+    if (!data)
+      return (
+        <Box textAlign="center" flexGrow={1}>
+          <Loading />
+        </Box>
+      )
+
+    if (!data.notice) return <div>Notice not found.</div>
+
+    const { notice } = data
+
+    return (
+      <>
+        <Grid item xs={12}>
+          <PageTitle>{notice.title}</PageTitle>
+        </Grid>
+        <Grid item xs={12}>
+          <StyledPaper elevation={0} variant="outlined">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: notice.content_html
+              }}
+            />
+          </StyledPaper>
+          <SectionTitle>Last updated</SectionTitle>
+          <StyledPaper elevation={0} variant="outlined">
+            {formatDate(notice.updated_at)}
+          </StyledPaper>
+        </Grid>
+      </>
+    )
+  }
+
+  return (
+    <Layout stats={data?.site_stats}>
+      <Head>
+        <title>Linkvan</title>
+      </Head>
+      <Container maxWidth="md">
+        <Grid container>
+          <Grid item xs={12}>
+            <NavBar />
+          </Grid>
+          {getContent()}
+        </Grid>
+      </Container>
+    </Layout>
+  )
+}
+
+export default NoticePage
