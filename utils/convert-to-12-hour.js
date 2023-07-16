dcdae8b7e33aa0ca5084e78aa68891ca95db8c9a
Andre Souza <andresouza4@gmail.com>
2023-07-16 10:03:28 -0700

    fix: add extra zero to time

--- utils/convert-to-12-hour.js
+++ utils/convert-to-12-hour.js
@@ -2,6 +2,7 @@ const convertTo12Hour = (time) => {
   let result = time.split(':')
   const ampm = Number(result[0]) <= 12 ? 'am' : 'pm'
   result[0] = String(Number(result[0]) % 12)
+  result = result.map((time) => time.padStart(2, '0'))
   return `${result.join(':')}${ampm}`
 }
 
