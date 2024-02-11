ec2d8540a3d4acbe0d0cc02e6b6d4aae928863ed
Andre Souza <andresouza4@gmail.com>
2024-02-11 10:50:47 -0800

    chore: pass user location in the header requests

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,6 +1,13 @@
-const fetcher = (url) =>
-  fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`).then((res) =>
+const fetcher = async (url) => {
+  const userLocation = sessionStorage.getItem("userLocation");
+
+  return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`, {
+    headers: {
+      "User-Location": userLocation
+    }
+  }).then((res) => 
     res.json()
   )
+}
 
 export default fetcher
